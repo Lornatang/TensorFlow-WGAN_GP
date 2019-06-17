0decd50c7cfbd6e336792b55153d10958ceb1ed5
Lornatang <shiyipaisizuo@163.com>
2019-06-17 21:52:36 +0800

    Update generate.py

--- models/generate.py
+++ models/generate.py
@@ -26,35 +26,35 @@ def make_generator_model():
 
   """
   model = tf.keras.Sequential()
-  model.add(layers.Dense(7 * 7 * 256,
+  model.add(layers.Dense(7 * 7 * 64,
                          use_bias=False,
                          input_shape=(100,)))
   model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
+  model.add(layers.ReLU())
 
-  model.add(layers.Reshape((7, 7, 256)))
-  assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+  model.add(layers.Reshape((7, 7, 64)))
+  assert model.output_shape == (None, 7, 7, 64)  # Note: None is the batch size
 
-  model.add(layers.Conv2DTranspose(128, (5, 5),
+  model.add(layers.Conv2DTranspose(64, (3, 3),
+                                   strides=(2, 2),
+                                   padding='same',
+                                   use_bias=False))
+  assert model.output_shape == (None, 7, 7, 64)
+  model.add(layers.BatchNormalization())
+  model.add(layers.ReLU())
+
+  model.add(layers.Conv2DTranspose(32, (3, 3),
+                                   strides=(2, 2),
+                                   padding='same',
+                                   use_bias=False))
+  assert model.output_shape == (None, 14, 14, 32)
+  model.add(layers.BatchNormalization())
+  model.add(layers.ReLU())
+
+  model.add(layers.Conv2DTranspose(1, (3, 3),
                                    strides=(1, 1),
                                    padding='same',
-                                   use_bias=False))
-  assert model.output_shape == (None, 7, 7, 128)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-
-  model.add(layers.Conv2DTranspose(64, (5, 5),
-                                   strides=(2, 2),
-                                   padding='same',
-                                   use_bias=False))
-  assert model.output_shape == (None, 14, 14, 64)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-
-  model.add(layers.Conv2DTranspose(1, (5, 5),
-                                   strides=(2, 2),
-                                   padding='same',
-                                   activation='tanh',
+                                   activation=tf.nn.sigmoid,
                                    use_bias=False))
   assert model.output_shape == (None, 28, 28, 1)
 
