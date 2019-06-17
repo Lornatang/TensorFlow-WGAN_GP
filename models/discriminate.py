f83313cd4ed796dc15ed0b74751ae50a543ea7db
Lornatang <shiyipaisizuo@163.com>
2019-06-17 17:34:02 +0800

    The structure of generation antagonism model is modified to DCGAN stable structure

--- models/discriminate.py
+++ models/discriminate.py
@@ -26,18 +26,16 @@ def make_discriminator_model():
 
   """
   model = tf.keras.Sequential()
-  model.add(layers.Conv2D(64, (3, 3),
-                          strides=(2, 2),
-                          padding='same',
-                          activation=tf.nn.relu,
+  model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
                           input_shape=[32, 32, 3]))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
 
-  model.add(layers.Conv2D(128, (3, 3),
-                          strides=(2, 2),
-                          padding='same',
-                          activation=tf.nn.relu))
+  model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+  model.add(layers.LeakyReLU())
+  model.add(layers.Dropout(0.3))
 
   model.add(layers.Flatten())
-  model.add(layers.Dense(1, activation=tf.nn.sigmoid))
+  model.add(layers.Dense(1))
 
   return model
--- models/generate.py
+++ models/generate.py
@@ -26,27 +26,24 @@ def make_generator_model():
 
   """
   model = tf.keras.Sequential()
-  model.add(layers.Dense(8 * 8 * 128,
-                         activation=tf.nn.relu,
-                         use_bias=False,
-                         input_shape=[128, ]))
+  model.add(layers.Dense(8 * 8 * 256, use_bias=False, input_shape=(100,)))
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
 
-  model.add(layers.Reshape((8, 8, 128)))
+  model.add(layers.Reshape((8, 8, 256)))
+  assert model.output_shape == (None, 8, 8, 256)  # Note: None is the batch size
 
-  model.add(layers.Conv2DTranspose(128, (3, 3),
-                                   strides=(2, 2),
-                                   padding='same',
-                                   activation=tf.nn.relu,
-                                   use_bias=False))
-  model.add(layers.Conv2DTranspose(64, (3, 3),
-                                   strides=(2, 2),
-                                   padding='same',
-                                   activation=tf.nn.relu,
-                                   use_bias=False))
-  model.add(layers.Conv2DTranspose(3, (3, 3),
-                                   strides=(1, 1),
-                                   padding='same',
-                                   activation=tf.nn.sigmoid,
-                                   use_bias=False))
+  model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+  assert model.output_shape == (None, 8, 8, 128)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+  assert model.output_shape == (None, 16, 16, 64)
+  model.add(layers.BatchNormalization())
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+  assert model.output_shape == (None, 32, 32, 3)
 
   return model
