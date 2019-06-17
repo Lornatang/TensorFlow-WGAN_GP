d6ede4940f91964ef5a15ae35c8d8f500d2dd32f
Lornatang <shiyipaisizuo@163.com>
2019-06-17 21:22:32 +0800

    Update train.py

--- train.py
+++ train.py
@@ -35,9 +35,6 @@ EPOCHS = 50
 noise_dim = 100
 num_examples_to_generate = 16
 
-# We will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
-seed = tf.random.normal([num_examples_to_generate, 1, 1, noise_dim])
 
 # create dir
 if not os.path.exists(save_path):
@@ -64,7 +61,7 @@ checkpoint_dir, checkpoint, checkpoint_prefix = save_checkpoints(generator,
 # This annotation causes the function to be "compiled".
 @tf.function
 def train_step(images):
-  noise = tf.random.normal([BATCH_SIZE, 1, 1, noise_dim])
+  noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
   with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
     generated_images = generator(noise, training=True)
@@ -100,6 +97,9 @@ def train(dataset, epochs):
     dataset: mnist dataset or cifar10 dataset.
     epochs: number of iterative training.
   """
+
+  seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
   for epoch in range(epochs):
     start = time.time()
 
