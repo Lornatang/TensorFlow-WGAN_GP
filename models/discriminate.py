affaadbb4d941701a66a10009c785891e6578dd9
Lornatang <shiyipaisizuo@163.com>
2019-06-17 17:54:51 +0800

    Update discriminate.py

--- models/discriminate.py
+++ models/discriminate.py
@@ -26,16 +26,18 @@ def make_discriminator_model():
 
   """
   model = tf.keras.Sequential()
-  model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+  model.add(layers.Conv2D(64, (3, 3),
+                          strides=(2, 2),
+                          padding='same',
+                          activation=tf.nn.relu,
                           input_shape=[32, 32, 3]))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
 
-  model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
+  model.add(layers.Conv2D(128, (3, 3),
+                          strides=(2, 2),
+                          padding='same',
+                          activation=tf.nn.relu))
 
   model.add(layers.Flatten())
-  model.add(layers.Dense(1))
+  model.add(layers.Dense(1, activation=tf.nn.sigmoid))
 
   return model
