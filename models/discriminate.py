f862af3cbc4d9680efc41e08ca1f3dec2e801fdd
Lornatang <shiyipaisizuo@163.com>
2019-06-18 20:05:00 +0800

    Update discriminate.py

--- models/discriminate.py
+++ models/discriminate.py
@@ -26,11 +26,11 @@ def make_discriminator_model():
 
   """
   model = tf.keras.Sequential()
+  model.add(layers.InputLayer(input_shape=[28, 28, 1]))
   model.add(layers.Conv2D(64, (3, 3),
                           strides=(2, 2),
                           padding='same',
-                          activation=tf.nn.relu,
-                          input_shape=[28, 28, 1]))
+                          activation=tf.nn.relu))
 
   model.add(layers.Conv2D(128, (3, 3),
                           strides=(2, 2),
